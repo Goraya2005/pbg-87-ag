06bff5b5e5b6e7eef47709b401d902ed86d83963
goraya2005 <goraya2005@gmail.com>
2025-05-26 06:01:04 +0500

    May 25, 2025

--- components/glimps.tsx
+++ components/glimps.tsx
@@ -16,12 +16,12 @@ const sections: Section[] = [
   {
     date: 'May 25, 2025',
     folder: '/images/25-05-2025',
-    images: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg'],
+    images: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', , '9.jpg', '10.jpg',  '11.jpg', '12.jpg'],
   },
   {
     date: 'April 27, 2025',
     folder: '/images/27-04-2025',
-    images: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '10.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg', '21.jpg', '22.jpg', '23.jpg', '24.jpg', '25.jpg', '26.jpg', '27.jpg', '28.jpg', '29.jpg', '30.jpg'],
+    images: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg',  '9.jpg', '10.jpg',  '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg', '21.jpg', '22.jpg', '23.jpg', '24.jpg', '25.jpg', '26.jpg', '27.jpg', '28.jpg', '29.jpg', '30.jpg'],
   },
   {
     date: 'November 24, 2024',
