cd0a3b26b106934540c6af7c44fe8de6f155e503
goraya2005 <goraya2005@gmail.com>
2025-05-26 06:17:41 +0500

    Images added

--- components/glimps.tsx
+++ components/glimps.tsx
@@ -16,7 +16,7 @@ const sections: Section[] = [
   {
     date: 'May 25, 2025',
     folder: '/images/25-05-2025',
-    images: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', , '9.jpg', '10.jpg',  '11.jpg', '12.jpg'],
+    images: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg',  '11.jpg', '12.jpg'],
   },
   {
     date: 'April 27, 2025',
