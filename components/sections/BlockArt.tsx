63d22d54c025e87c6a0125374ebea68fbda98ba2
goraya2005 <goraya2005@gmail.com>
2025-07-02 04:06:03 +0500

    New Home Page

--- components/sections/BlockArt.tsx
+++ components/sections/BlockArt.tsx
@@ -6,7 +6,7 @@ interface BlockArtProps {
 }
 
 export default function BlockArt({ 
-  title = "PBG Class of &apos;87 - Faisalabad Chapter",
+  title = "Plant Breeding & Genetics 1987 - Faisalabad Chapter",
   subtitle = "Decades later, our bond remains unbroken. Celebrating a lifetime of friendship, respect, and shared memories from our days at the University of Agriculture, Faisalabad."
 }: BlockArtProps) {
   return (
